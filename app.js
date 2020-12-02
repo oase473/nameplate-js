8d897314e63cebf333ae60a6e706636bfead0e29
Tim Bliefert <timbliefert@Tims-Air.fritz.box>
2020-12-02 21:05:17 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+// jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static("public"));
+
+app.get("/", function(req,res){
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(3000, function(){
+    console.log("Server is listining on port 3000");
+});
